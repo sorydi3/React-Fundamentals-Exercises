3fa303c8edc3735dc1504de3c52177ca4578f277
ibradi3 <ibrahimasory22@hotmail.com>
2022-12-29 10:44:08 +0100

    exercice usestate done

--- L3. State Management/3. Managing State/Starter/src/App.js
+++ L3. State Management/3. Managing State/Starter/src/App.js
@@ -1,12 +1,20 @@
 import corgi from "./corgi.jpg";
 import "./App.css";
+import { useState } from "react";
 
 const App = () => {
+  const [likes, setLikes] = useState(0);
+
+  const handleclick = () => {
+    setLikes(likes + 1);
+  };
+
   return (
     <div className="container">
       <h2>Like this photo!</h2>
       <img src={corgi} alt="Corgi"></img>
-      <p>Amount of likes: </p>
+      <p>Amount of likes: {likes} </p>
+      <button onClick={handleclick}>Like</button>
     </div>
   );
 };
